629897f0c2a4687df37927615ecec4ad2b6057e7
MOBOLA MORONTO <t2wycemo@gmail.com>
2021-02-24 19:46:01 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
 class MovieCard extends React.Component {
   render() {
-    
+    return (
+      <div className="movie-card">
+      <img
+        src={this.props.poster}
+        alt={this.props.title}
+      />
+      <h2>{this.props.title}</h2>
+      <small>Genres: {this.props.genres.join(', ')}</small>
+      </div>
+      );
   }
 }
 
-const jurassicWorldGenres = ['Action', 'Adventure', 'Science Fiction', 'Thriller']
+const jurassicWorldGenres = ['Action', 'Adventure', 'Science Fiction', 'Thriller'];
+
+ReactDOM.render(
+  <MovieCard
+    title ="Jurassic World"
+    genres={jurassicWorldGenres}
+  />,
+  document.getElementById('root')
+  );
